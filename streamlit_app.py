55b6f515555f28016511304bffc1916ef09930c3
Clydepn1997 <136773427+Clydepn1997@users.noreply.github.com>
2023-06-19 17:12:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,6 @@ except URLError as e:
 streamlit.stop()
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
 streamlit.header("The fruit load list contains:")
 #snowflake related functions
